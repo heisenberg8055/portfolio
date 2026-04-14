3b6b3f5d5e662adc55e4261a54100ae63cd0e08e
Yeswanth <yeswanthsai.inturi@gmail.com>
2026-04-15 02:40:32 +0530

    fix: correct resume link filename to match actual file
    - Update nav.js resume link from /Yeswanth_Inturi_Resume.pdf to /Yeswanth_Inturi.pdf
    - Update menu.js resume link from /Yeswanth_Inturi_Resume.pdf to /Yeswanth_Inturi.pdf
    - Filename now matches the actual resume file in static directory

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/Yeswanth_Inturi_Resume.pdf" className="resume-link">
+            <a href="/Yeswanth_Inturi.pdf" className="resume-link">
               Resume
             </a>
           </nav>
